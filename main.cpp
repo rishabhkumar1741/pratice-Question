4cd38b5c890d20d74bad4a67c6fd6a509e44dd21
Rishabh Kumar <1741Rishabh@gmail.com>
2021-07-26 22:59:00 +0530

    first commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "praticeQuestion.h"
+using namespace std;
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+    
+    pattern::ris();
+
+    
+    return 0 ;
+}
---
+++ praticeQuestion.h
@@ -0,0 +1,142 @@
+#include <iostream>
+using namespace std;
+namespace Ris
+{
+    int sums(int a, int b)
+    {
+        cout << "the sum is a fuck";
+        cout << a + b;
+        return 0;
+    }
+    int takeinput()
+    {
+        int x;
+        cin >> x;
+        while (x >= 0)
+        {
+            cin >> x;
+        }
+
+        cout << "the ans is ";
+        cout << x;
+        return 0;
+    }
+    //  learing about continue and break statment
+    int count(int x)
+    {
+        for (int i = 0; i <= 10; i++)
+        {
+            if (i % 2 == 0)
+            {
+                continue;
+            }
+            else if (i == 5)
+            {
+                break;
+            }
+
+            else
+            {
+                cout << x * i;
+                cout << endl;
+            }
+        }
+        return 0;
+    }
+    // switch statment
+
+}
+
+void calculater()
+{
+    int x;
+    int y;
+    cin >> x >> y;
+    char opertater;
+    cin >> opertater;
+    cout << "program is start and your operater is ";
+    cout << opertater;
+
+    switch (opertater)
+    {
+    case '+':
+        cout << "addition is ";
+        cout << x + y;
+        break;
+    case '-':
+        cout << "subtration is ";
+        cout << x - y;
+        break;
+    case '*':
+        cout << "multi is ";
+        cout << x * y;
+        break;
+    case '/':
+        cout << "divider is ";
+        cout << x / y;
+        break;
+
+    default:
+        break;
+    }
+}
+namespace operation
+{
+    // operation
+    void bitwise()
+    {
+        try
+        {
+            int a = 14;
+            cout << (a >> 1);
+        }
+        catch (const exception &e)
+        {
+            cerr << e.what() << '\n';
+        }
+    }
+
+}
+namespace pattern
+{
+    void hollow_rectangle()
+    {
+        short int row;
+        short int col;
+        cin >> row;
+        cin >> col;
+        for (int i = 1; i <= row; i++)
+        {
+            for (int j = 1; j <= col; j++)
+            {
+                if (i == 1 || j == 1 || j == col || i == row)
+                {
+                    cout << "*";
+                }
+                else
+                {
+                    cout << " ";
+                }
+            }
+            cout << endl;
+        }
+    }
+    void inverted_half_pyramid()
+    {
+        short int x ;
+        cin>>x;
+        for(int i =x ;i>0;i--)
+        {
+            for(int j=i ; j>0 ;j--)
+            {
+                cout<<"*";
+            }
+            cout<<endl;
+        }
+    }
+    void ris()
+    {
+        cout<<"i am working ";
+        
+    }
+}
